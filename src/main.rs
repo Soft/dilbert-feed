d4d02052e13df680466acb1ce40525905ac6b188
Samuel Laurén <samuel.lauren@iki.fi>
2018-01-28 23:39:25 +0200

    Error handling

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,24 @@
 extern crate atom_syndication;
 extern crate reqwest;
 extern crate select;
+extern crate failure;
 
 use atom_syndication::{Feed, Content};
 use std::io::BufReader;
 use select::document::Document;
 use select::predicate::Class;
+use failure::{Error, err_msg};
 
 const SOURCE_URL: &str = "http://dilbert.com/feed";
 
-fn extract_image_url(url: &str) -> String {
-    let response = reqwest::get(url).unwrap();
-    let response = Document::from_read(response).unwrap();
-    let image = response.find(Class("img-comic")).next().unwrap();
-    image.attr("src").unwrap().to_owned()
+fn extract_image_url(url: &str) -> Result<String, Error> {
+    let response = reqwest::get(url)?;
+    let response = Document::from_read(response)?;
+    let image = response.find(Class("img-comic"))
+        .next().ok_or(err_msg("missing image"))?;
+    Ok(image.attr("src")
+       .ok_or(err_msg("invalid image"))?
+       .to_owned())
 }
 
 fn create_content(url: &str) -> Content {
@@ -23,28 +28,37 @@ fn create_content(url: &str) -> Content {
     content
 }
 
-fn main() {
-    let response = reqwest::get(SOURCE_URL).unwrap();
+fn create_feed() -> Result<Feed, Error> {
+    let response = reqwest::get(SOURCE_URL)?;
     let response = BufReader::new(response);
 
     let mut feed = Feed::read_from(response)
-        .unwrap()
+        .map_err(|_| err_msg("invalid feed"))?
         .clone();
 
-    let entries: Vec<_> = feed.entries()
+    let entries: Result<Vec<_>, Error> = feed.entries()
         .iter()
         .cloned()
         .map(|mut entry| {
             let image_url = {
-                let link = entry.links().iter().next().unwrap();
+                let link = entry.links().iter().next()
+                    .ok_or(err_msg("missing link"))?;
                 extract_image_url(link.href())
             };
-            let content = create_content(&image_url);
+            let content = create_content(&image_url?);
             entry.set_content(content);
-            entry
+            Ok(entry)
         })
         .collect();
-    feed.set_entries(entries);
-    
-    println!("{}", feed.to_string());
+    feed.set_entries(entries?);
+    Ok(feed)
+}
+
+fn main() {
+    match create_feed() {
+        Ok(feed) => {
+            println!("{}", feed.to_string());
+        },
+        Err(err) => eprintln!("{}", err)
+    }
 }
